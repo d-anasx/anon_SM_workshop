def2ddde5ad1b89d1cec4eb04e98b36f2c527f7a
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-01-30 10:34:20 +0000

    i added factory and edited migrations

--- database/migrations/2026_01_29_193949_create_comments_table.php
+++ database/migrations/2026_01_29_193949_create_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,8 +13,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('comment');
-            $table->integer('post_id');
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreignId('post_id')->constrained();
             $table->timestamps();
         });
     }
