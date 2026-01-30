21c857ecf5279fe171917ae2ec3532dd10a3ffb2
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-01-30 09:24:17 +0000

    i added comment and posts migrations

--- database/migrations/2026_01_29_193949_create_comments_table.php
+++ database/migrations/2026_01_29_193949_create_comments_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('comment');
+            $table->integer('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
             $table->timestamps();
         });
     }
